cad998f6007db9115d52fa6bde223f3234cc6842
Björn Sterling <bjorn.sterling@gmail.com>
2025-06-04 20:17:30 +0200

    created a variable to keep track of the row

--- halfOfASquare.cpp
+++ halfOfASquare.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main()
 {
-    // create a variable named row to keep track of the number of rows for the half square and initial it with the value 5.
+    // create a variable named row of int type to keep track of the number of rows for the half square and initial it with the value 5.
+    int row = 5;
 
     // create a variable named width to keep track of the width of the each row and initial it with the value 5. 
 
